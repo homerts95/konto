1c00ce5fc73a49cb2a94e2d2d2a57e7bbafa81d6
Omiros Tsintilas <h.tsintilas@gmail.com>
2024-05-27 23:19:32 +0300

    user role migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -30,6 +30,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('tac');
+            $table->string('role')->default('user'); //user, admin, god
             $table->rememberToken();
             $table->timestamps();
         });

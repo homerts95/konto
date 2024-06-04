dc13264679768a2b891eeba0a56d193936aaf852
Omiros Tsintilas <h.tsintilas@gmail.com>
2024-06-04 20:47:52 +0300

    script for translations

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
-import i18n from 'laravel-vue-i18n/vite';
 
 export default defineConfig({
     build: {
@@ -22,13 +21,6 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-        }),
-        i18n({
-            // you can also change your langPath here
-            // langPath: 'locales'
-            additionalLangPaths: [
-                //'public/locales' // Load translations from this path too!
-            ]
         }),
 
     ],

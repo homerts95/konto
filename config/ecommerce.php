42bd22b61f9392111914575e62b36e819da1264f
Omiros Tsintilas <h.tsintilas@gmail.com>
2024-06-06 23:12:47 +0300

    changed it

--- config/ecommerce.php
+++ config/ecommerce.php
@@ -3,8 +3,8 @@
 return [
 
     // Stripe Key
-    'currency' => env('CURRENCY'),
-    'enable_stripe' => env('STRIPE_SECRET_KEY', false) ? true : false,
-    'enable_whatsapp' => env('WHATSAPP_NUMBER', false) ? true : false,
+    'currency' => env('CURRENCY', 'EUR'),
+    'enable_stripe' => (bool)env('STRIPE_SECRET_KEY', false),
+    'enable_whatsapp' => (bool)env('WHATSAPP_NUMBER', false),
     'whatsapp_number' => env('WHATSAPP_NUMBER', ''),
 ];
